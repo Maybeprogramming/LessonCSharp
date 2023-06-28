02d599b98ac35ee6389bc87f48828d65d30deaa9
DELL <crazy_63@mail.ru>
2023-06-28 16:05:47 +0300

    Сортировка чисел

---
+++ Lesson_25/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Lesson_25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers;
+            Random random = new Random();
+            int maxNumbers = 1000;
+            int maxArrayLength = 30;
+            numbers = new int[maxArrayLength];
+
+            Console.Title = "Программа для сортировки массива от меньшего до большего \"методом выбора\"";
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = random.Next(maxNumbers);
+            }
+
+            Console.WriteLine($"Успешно сгенерирован массив длинной: {numbers.Length} чисел");
+            Console.WriteLine("\nИсходный массив:");
+
+            foreach (int number in numbers)
+            {
+                Console.Write($"{number} ");
+            }
+
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                int minValue = i;
+
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    if (numbers[j] < numbers[minValue])
+                    {
+                        minValue = j;
+                    }
+                }
+
+                int tempNumbers = numbers[minValue];
+                numbers[minValue] = numbers[i];
+                numbers[i] = tempNumbers;
+            }
+
+            Console.WriteLine("\n\nПолученный массив:");
+
+            foreach (int number in numbers)
+            {
+                Console.Write($"{number} ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
