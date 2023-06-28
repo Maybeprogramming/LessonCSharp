d7b4939bc264b817669cc56583091813f314a938
DELL <crazy_63@mail.ru>
2023-06-28 14:21:27 +0300

    Скобочное выражение

---
+++ Lesson_18/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Lesson_18
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string userInputText;
+            int bracketsCount = 0;
+            int depthCount = 0;
+            int balanceValue = 0;
+            char bracketOpen = '(';
+            char bracketClose = ')';
+
+            Console.WriteLine("Введите строку из символов: \"(\" и \")\".");
+            userInputText = Console.ReadLine();
+
+            for (int i = 0; i < userInputText.Length; i++)
+            {
+                if (userInputText[i] == bracketOpen)
+                {
+                    bracketsCount++;
+                }
+                else if (userInputText[i] == bracketClose)
+                {
+                    if (i != userInputText.Length - 1 && userInputText[i + 1] != bracketOpen)
+                    {
+                        depthCount++;
+                    }
+
+                    bracketsCount--;
+                }
+
+                if (bracketsCount < balanceValue)
+                {
+                    break;
+                }
+            }
+
+            if (bracketsCount == balanceValue)
+            {
+                Console.WriteLine($"\"{userInputText}\" - cтрока корректная, максимальная глубина составляет: {depthCount + 1}");
+            }
+            else
+            {
+                Console.WriteLine($"\"{userInputText}\" - некорректная строка!");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
