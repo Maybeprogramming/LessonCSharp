6c93f1135b1fbc0e683d17bf63891610564175cc
DELL <crazy_63@mail.ru>
2023-06-28 11:47:54 +0300

    Последовательность

---
+++ Lesson_10/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Lesson_10
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int startNumber = 5;
+            int endNumber = 96;
+            int stepIteration = 7;
+
+            Console.Write("Последовательность чисел:");
+
+            for (int i = startNumber; i <= endNumber; i += stepIteration)
+            {
+                Console.Write(i + " ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
