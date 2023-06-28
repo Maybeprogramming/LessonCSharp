2ab6e81f27e07e52744728a3729e0527bcb8cde7
DELL <crazy_63@mail.ru>
2023-06-28 10:39:15 +0300

    Lesson_02

---
+++ Lesson_02/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Lesson_02
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int a = 10;
+            int b = 38;
+            int c = (31 - 5 * a) / b;
+            Console.WriteLine(c);
+
+            Console.ReadLine();
+        }
+    }
+}
