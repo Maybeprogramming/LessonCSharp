8ef76a11f6ea8bcd5aa564d23dd6ea0a923010c8
DELL <crazy_63@mail.ru>
2023-06-28 16:20:59 +0300

    READINT

--- Lesson_30/Program.cs
+++ Lesson_30/Program.cs
@@ -1,204 +1,57 @@
 ﻿namespace Lesson_30
 {
-    //Сделать игровую карту с помощью двумерного массива.
-    //Сделать функцию рисования карты.
-    //Помимо этого, дать пользователю возможность перемещаться по карте
-    //и взаимодействовать с элементами
-    //(например пользователь не может пройти сквозь стену)
-
-    //Все элементы являются обычными символами
-    //Brave New World
-
     internal class Program
     {
-        //123
-        //456
-
-
         static void Main()
         {
-            Console.CursorVisible = false;
+            Console.Title = "Программа для конвертации введенной строки в число";
 
-            char[,] map = ReadMap("map.txt");
-            int pacmanX = 0;
-            int pacmanY = 0;
-            int pacmanDX = 0;
-            int pacmanDY = 1;
-
-            bool isPlaying = true;
-            bool isAlive = true;
-
-            char player = '@';
-            char ghost = '$';
-
-            int ghostX = 0;
-            int ghostY = 0;
-
-            int allCherry = 0;
-            int collectCherry = 0;
-
-            FindSpawnPoint(map, ref pacmanX, ref pacmanY, player);
-            FindSpawnPoint(map, ref ghostX, ref ghostY, ghost);
-            AddCherryToMap(map);
-            allCherry = CalculateAllCherryInMap(map);
-            DrowMap(map);
-
-            while (isPlaying == true)
-            {
-                Console.SetCursorPosition(0, 20);
-                Console.WriteLine($"Собрано: {collectCherry}/{allCherry}");
-
-                if (Console.KeyAvailable == true)
-                {
-                    ConsoleKeyInfo key = Console.ReadKey(true);
-
-                    ChangeDirection(key, ref pacmanDX, ref pacmanDY);
-                }
-
-                if (map[pacmanX + pacmanDX, pacmanY + pacmanDY] != '#')
-                {
-                    Move(ref pacmanX, ref pacmanY, pacmanDX, pacmanDY);
-
-                    CollectDots(map, pacmanX, pacmanY, ref collectCherry);
-                }
-
-                Task.Delay(100).Wait();
-
-                if (collectCherry == allCherry)
-                {
-                    isPlaying = false;
-                }
-            }
-
-            if (collectCherry == allCherry)
-            {
-                Console.SetCursorPosition(0, 25);
-                Console.WriteLine("Вы победили");
-            }
-
-            Console.ReadLine();
+            StartToParse();
+            PrintText($"\nРаботы программы завершена", ConsoleColor.Green);
         }
 
-        static void CollectDots(char[,] map, int pacmanX, int pacmanY, ref int collectCherry)
+        static void StartToParse()
         {
-            if (map[pacmanX, pacmanY] == '.')
+            bool isTryParse = false;
+            string requestNumber = "Введите число: ";
+            string continueMessage = "\n\nНажмите любую клавишу чтобы продолжить...";
+            string userInput;
+            int number;
+
+            while (isTryParse == false)
             {
-                collectCherry++;
-                map[pacmanX, pacmanY] = ' ';
+                Console.Clear();
+                PrintText(requestNumber);
+                userInput = Console.ReadLine();
+                number = ParseStringToInt(userInput, out bool isParseToInt);
+
+                if (isParseToInt == true)
+                {
+                    PrintText($"\nВы ввели число: {number}", ConsoleColor.Green);
+                    isTryParse = isParseToInt;
+                }
+                else
+                {
+                    PrintText($"\nВы ввели не число: {userInput}", ConsoleColor.Red);
+                }
+
+                PrintText(continueMessage);
+                Console.ReadLine();
             }
         }
 
-        static void Move(ref int X, ref int Y, int DX, int DY)
+        static int ParseStringToInt(string userInput, out bool isParseToInt)
         {
-            Console.SetCursorPosition(Y, X);
-            Console.Write(" ");
-
-            X += DX;
-            Y += DY;
-
-            Console.SetCursorPosition(Y, X);
-            Console.Write('@');
+            isParseToInt = int.TryParse(userInput, out int result);
+            return result;
         }
 
-        static void ChangeDirection(ConsoleKeyInfo key, ref int DX, ref int DY)
+        static void PrintText(string text, ConsoleColor color = ConsoleColor.White)
         {
-            switch (key.Key)
-            {
-                case ConsoleKey.UpArrow:
-                    DX = -1;
-                    DY = 0;
-                    break;
-
-                case ConsoleKey.DownArrow:
-                    DX = 1;
-                    DY = 0;
-                    break;
-
-                case ConsoleKey.LeftArrow:
-                    DX = 0;
-                    DY = -1;
-                    break;
-
-                case ConsoleKey.RightArrow:
-                    DX = 0;
-                    DY = 1;
-                    break;
-            }
-        }
-
-        static char[,] ReadMap(string mapName)
-        {
-            string[] newFile = File.ReadAllLines($"Map\\{mapName}");
-            char[,] map = new char[newFile.Length, newFile[0].Length];
-
-            for (int i = 0; i < map.GetLength(0); i++)
-            {
-                for (int j = 0; j < map.GetLength(1); j++)
-                {
-                    map[i, j] = newFile[i][j];
-                }
-            }
-
-            return map;
-        }
-
-        static void AddCherryToMap(char[,] map)
-        {
-            for (int i = 0; i < map.GetLength(0); i++)
-            {
-                for (int j = 0; j < map.GetLength(1); j++)
-                {
-                    if (map[i, j] == ' ')
-                    {
-                        map[i, j] = '.';
-                    }
-                }
-            }
-        }
-
-        static int CalculateAllCherryInMap(char[,] map)
-        {
-            int cherryInMap = 0;
-            for (int i = 0; i < map.GetLength(0); i++)
-            {
-                for (int j = 0; j < map.GetLength(1); j++)
-                {
-                    if (map[i, j] == '.')
-                    {
-                        cherryInMap++;
-                    }
-                }
-            }
-
-            return cherryInMap;
-        }
-
-        static void FindSpawnPoint(char[,] map, ref int posX, ref int posY, char characterSymbol)
-        {
-            for (int i = 0; i < map.GetLength(0); i++)
-            {
-                for (int j = 0; j < map.GetLength(1); j++)
-                {
-                    if (map[i, j] == characterSymbol)
-                    {
-                        posX = i;
-                        posY = j;
-                        break;
-                    }
-                }
-            }
-        }
-
-        static void DrowMap(char[,] map)
-        {
-            for (int i = 0; i < map.GetLength(0); i++)
-            {
-                for (int j = 0; j < map.GetLength(1); j++)
-                {
-                    Console.Write(map[i, j]);
-                }
-                Console.WriteLine();
-            }
+            ConsoleColor defaultColor = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Console.Write(text);
+            Console.ForegroundColor = defaultColor;
         }
     }
 }
--- Lesson_32/Program.cs
+++ Lesson_32/Program.cs
@@ -4,57 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Random random = new Random();
-            int[,] numbers;
-            int rowCount;
-            int collumCount;
-            int minRandNumber = -9;
-            int maxRandNumber = 9;
-            int zeroValue = 0;
-            int maxNumber = int.MinValue;
-
-            Console.WriteLine("Программа по работе с двухмерной матрицой.");
-            Console.Write("Введите колличество строк: ");
-            rowCount = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Введите количество столбцов: ");
-            collumCount = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine($"\nИсходная матрица чисел, размерностью: {rowCount}х{collumCount}\n");
-            numbers = new int[rowCount, collumCount];
-
-            for (int i = 0; i < numbers.GetLength(0); i++)
-            {
-                for (int j = 0; j < numbers.GetLength(1); j++)
-                {
-                    numbers[i, j] = random.Next(minRandNumber, maxRandNumber);
-                    Console.Write($" {numbers[i, j]} \t");
-
-                    if (numbers[i, j] > maxNumber)
-                    {
-                        maxNumber = numbers[i, j];
-                    }
-                }
-
-                Console.WriteLine();
-            }
-
-            Console.WriteLine("\nПолученная матрица:\n");
-
-            for (int i = 0; i < numbers.GetLength(0); i++)
-            {
-                for (int j = 0; j < numbers.GetLength(1); j++)
-                {
-                    if (numbers[i, j] == maxNumber)
-                    {
-                        numbers[i, j] = zeroValue;
-                    }
-
-                    Console.Write($" {numbers[i, j]} \t");
-                }
-
-                Console.WriteLine();
-            }
-
-            Console.WriteLine($"\nМаксимальное число в матрице равно: {maxNumber}");
+            //Реализуйте функцию Shuffle, которая перемешивает элементы массива в случайном порядке.
+            //Kansas city shuffle
             Console.ReadKey();
         }
     }
