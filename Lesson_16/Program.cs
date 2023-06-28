ba439ba7950f61c6fa00d841c3897c2f29d2ab48
DELL <crazy_63@mail.ru>
2023-06-28 14:03:22 +0300

    Кратные числа

---
+++ Lesson_16/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Lesson_16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            int startRandomNumber = 1;
+            int endRandomNumber = 27;
+            int startNaturalNumber = 100;
+            int endNaturalNumber = 999;
+            int amountNaturalNumbers = 0;
+            Random random = new Random();
+
+            number = random.Next(startRandomNumber, endRandomNumber + 1);
+            Console.WriteLine($"Число N = {number}");
+
+            for (int i = number; i <= endNaturalNumber; i += number)
+            {
+                if (i >= startNaturalNumber)
+                {
+                    amountNaturalNumbers++;
+                }
+            }
+
+            Console.WriteLine($"Колличество трехзначных натуральных чисел кратных {number} равняется: {amountNaturalNumbers}");
+            Console.ReadLine();
+        }
+    }
+}
