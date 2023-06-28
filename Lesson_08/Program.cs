575496bd42ce63210690941433db9c4f182aa74e
DELL <crazy_63@mail.ru>
2023-06-28 11:41:45 +0300

    Освоение циклов

---
+++ Lesson_08/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Lesson_08
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string userMessage;
+            int printCount;
+
+            Console.WriteLine("Добропожаловать в типографию!");
+            Console.WriteLine("Введите ваше сообщение для печати:");
+            userMessage = Console.ReadLine();
+            Console.Write("Сколько копий хотите распечатать? ");
+            printCount = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < printCount; i++)
+            {
+                Console.WriteLine(userMessage);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
