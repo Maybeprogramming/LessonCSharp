6ee37cdd8a12c1c2eff20bb4235304bc954b0899
DELL <crazy_63@mail.ru>
2023-06-28 14:17:56 +0300

    Степень двойки

---
+++ Lesson_17/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Lesson_17
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int randomNumber;
+            int totalNumber;
+            int number = 2;
+            int minDegreeCount = 1;
+            int maxRandomNumber = 65;
+            Random random = new Random();
+            randomNumber = random.Next(maxRandomNumber);
+            totalNumber = number;
+
+            while (randomNumber >= totalNumber)
+            {
+                totalNumber *= number;
+                minDegreeCount++;
+            }
+
+            Console.WriteLine($"Число {number} в степени {minDegreeCount} превосходит заданное число {randomNumber}");
+            Console.WriteLine($"Результат: {totalNumber} > {randomNumber}");
+            Console.WriteLine("\nРабота программы завершена.");
+            Console.ReadKey();
+        }
+    }
+}
