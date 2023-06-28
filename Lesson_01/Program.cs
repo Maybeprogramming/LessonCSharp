85891c4fb7542ae06a93925779c5493ab76a14de
DELL <crazy_63@mail.ru>
2023-06-28 10:30:45 +0300

    Lesson_01

---
+++ Lesson_01/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Lesson_01
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string name;
+            string surname;
+            string message;
+            string enemyDescription;
+            string inputMessage;
+            string summaryMessage;
+            string weaponDescription;
+
+            char simbol;
+            char letter;
+
+            byte age;
+            byte bagCapacity;
+
+            uint health;
+            uint forcePlayer;
+            uint stamina;
+
+            int armor;
+            int weaponPrice;
+            int distanceToEnemy;
+
+            ulong money;
+
+            long time;
+
+            ushort playerTokens;
+            ushort healthPotions;
+
+            float maxSpeed;
+            float damage;
+
+            double maxMoney;
+
+            bool isMove;
+            bool isEating;
+            bool isAlive;
+            bool isDead;
+            bool isGround;
+            bool isEnable;
+            bool isJump;
+            bool isRun;
+            bool isOpen;
+            bool isClose;
+        }
+    }
+}
