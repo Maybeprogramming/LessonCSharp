7e4000b3a6ac0f4274a27d820822eea8455bbee6
DELL <crazy_63@mail.ru>
2023-06-28 11:37:55 +0300

    7

---
+++ Lesson_07/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Lesson_07
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int patientsInQueue;
+            int waitingTime;
+            int patientReceptionTime = 10;
+            int minutesPerHour = 60;
+            int waitingHours;
+            int waitingMinutes;
+
+            Console.WriteLine("Добро пожаловать в поликлинику!!!");
+            Console.Write("Сколько людей в очереди на приём перед вами? ");
+            patientsInQueue = Convert.ToInt32(Console.ReadLine());
+
+            waitingTime = patientsInQueue * patientReceptionTime;
+            waitingHours = waitingTime / minutesPerHour;
+            waitingMinutes = waitingTime % minutesPerHour;
+
+            Console.WriteLine($"Вы должны отстоять в очереди {waitingHours} часа {waitingMinutes} минут");
+            Console.ReadLine();
+        }
+    }
+}
