46671d854b4eb85391e33674c576527f8df7633c
Maybeonce <a-u-rom@yandex.ru>
2023-09-14 23:37:21 +0400

    Гладиаторские бои

--- Lesson_45/Program.cs
+++ Lesson_45/Program.cs
@@ -1,9 +1,132 @@
 ﻿namespace Lesson_45
 {
+    using static Display;
+
     class Program
     {
         static void Main()
         {
+            Fighter[] fighters =
+            {
+                new Warrior("Володя", 100, 20, 20),
+                new Hunter ("Ева", 80, 25, 15)
+            };
+
+            foreach (var fighter in fighters)
+            {
+                PrintColorText(fighter.ShowInfo() + "\n", '<', '>', ConsoleColor.Green);
+                
+                if (fighter is Warrior warrior)
+                    warrior.Healing();
+
+                if (fighter is Hunter hunter)
+                    hunter.SummonPet();
+
+                Console.WriteLine(new string('-', 50));
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    abstract class Fighter
+    {
+        public Fighter(string name, int health, int damage, int armor)
+        {
+            Name = name;
+            Health = health;
+            Damage = damage;
+            Armor = armor;
+        }
+
+        public string Name { get; private set; }
+        public int Health { get; private set; }
+        public int Damage { get; private set; }
+        public int Armor { get; private set; }
+
+        public abstract void TakeDamage(int damage);
+
+        public virtual string ShowInfo()
+        {
+            return $"Имя бойца: {Name}. Характеристики: здоровье <{Health}>, наносимый урон <{Damage}>, показатель брони <{Armor}>";
+        }
+    }
+
+    class Warrior : Fighter
+    {
+        public Warrior(string name, int health, int damage, int armor) : base(name, health, damage, armor)
+        {
+
+        }
+
+        public override void TakeDamage(int damage)
+        {
+            Console.WriteLine($"Я {Name} - получаю урон {damage}");
+        }
+
+        public void Healing()
+        {
+            Console.WriteLine("Прилив священного огня! Исцели моё тело и душу!");
+        }
+    }
+
+    class Hunter : Fighter
+    {
+        public Hunter(string name, int health, int damage, int armor) : base(name, health, damage, armor)
+        {
+        }
+
+        public override void TakeDamage(int damage)
+        {
+            Console.WriteLine($"Я {Name} - получаю урон {damage}");            
+        }
+
+        public void SummonPet()
+        {
+            Console.WriteLine("Призываю грозную морскую свинку!");
+        }
+    }
+
+    static class Display
+    {
+        public static void PrintColorText(string text,char startChar, char endChar, ConsoleColor consoleColor = ConsoleColor.White)
+        {
+            bool isStartChar = false;
+            bool isFinishChar = false;
+            ConsoleColor defaulColor = Console.ForegroundColor;
+
+            foreach (char symbol in text)
+            {
+                if(symbol.Equals(startChar)) 
+                {
+                    isStartChar = true;
+                    Console.ForegroundColor = consoleColor;
+                    Console.Write(symbol);
+                }
+                else if(symbol.Equals(endChar))
+                {
+                    Console.Write(symbol);
+                    isFinishChar = true;
+                    Console.ForegroundColor = defaulColor;
+                }
+                else if (isStartChar == true & isFinishChar == false)
+                {
+                    Console.ForegroundColor = consoleColor;
+                    Console.Write(symbol);
+                }
+                else if(isStartChar == true & isFinishChar == true)
+                {
+                    isFinishChar = false;
+                    isStartChar = false;
+                    Console.ForegroundColor = defaulColor;
+                    Console.Write(symbol);
+                }
+                else
+                {
+                    Console.ForegroundColor = defaulColor;
+                    Console.Write(symbol);
+                }                
+            }
         }
     }
 }
