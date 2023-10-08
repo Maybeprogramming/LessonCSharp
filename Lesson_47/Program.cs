293c9f986315394e26bb3b8d1fefed0698104c6d
Maybeonce <a-u-rom@yandex.ru>
2023-10-08 23:13:11 +0300

    !

--- Lesson_47/Program.cs
+++ Lesson_47/Program.cs
@@ -1,10 +1,66 @@
 ﻿namespace Lesson_47
 {
-    internal class Program
+    class Program
     {
         static void Main()
         {
         }
+    }
+
+    class BattleField
+    {
+        Squad squad1 = new Squad();
+        Squad squad2 = new Squad();
+
+        public void BeginWar()
+        {
+
+        }
+    }
+
+    class Squad
+    {
+
+    }
+
+    abstract class Fighter
+    {
+
+    }
+
+    class Stormtrooper : Fighter
+    {
+
+    }
+
+    class Sniper: Fighter
+    {
+
+    }
+
+    class Paratrooper: Fighter
+    {
+
+    }
+
+    class Scout: Fighter
+    {
+
+    }
+
+    class Heavy : Fighter
+    {
+
+    }
+
+    class GrenadeLauncher: Fighter
+    {
+
+    }
+
+    class Medic: Fighter
+    {
+
     }
 }
 
@@ -19,3 +75,11 @@
 //такие как повышенная сила.
 //Побеждает та страна, во взводе которой остались выжившие бойцы.
 //Не важно, какой будет бой, рукопашный, стрелковый.
+
+//Штурмовик - Stormtrooper
+//Снайпер - Sniper
+//Десантник - Paratrooper
+//Разведчик - Scout
+//Пулеметчик - Heavy
+//Гранатометчик - Grenade launcher
+//Медик - Medic
