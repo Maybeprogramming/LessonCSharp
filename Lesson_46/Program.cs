d292a15205fef3c58012f3e785a456ed6d285e0d
Maybeonce <a-u-rom@yandex.ru>
2023-10-08 20:11:45 +0300

    !

--- Lesson_46/Program.cs
+++ Lesson_46/Program.cs
@@ -1,9 +1,14 @@
 ﻿namespace Lesson_46
 {
+    using static Randomaizer;
+    using static UserInput;
+    using static Display;
+
     class Program
     {
         static void Main()
         {
+
         }
     }
 
@@ -11,6 +16,17 @@
     {
         private Seller? _seller;
         private Queue<Buyer>? _buyers;
+
+        public Market()
+        {
+            _seller = new();
+            _buyers = new();
+        }
+
+        public void Work()
+        {
+
+        }
     }
 
     class Seller
@@ -34,24 +50,59 @@
         private List<Product> _products;
     }
 
-    class  Product
+    class Product
     {
-        
+        public Product(string description, int price)
+        {
+            Description = description;
+            Price = price;
+        }
+
+        public string Description { get; }
+        public int Price { get; }
     }
 
-    static class Randomizer
+    static class Randomaizer
     {
+        private static readonly Random s_random;
 
+        static Randomaizer()
+        {
+            s_random = new();
+        }
+
+        public static int GenerateRandomNumber(int minValue, int maxValue)
+        {
+            return s_random.Next(minValue, maxValue);
+        }
     }
 
     static class UserInput
     {
+        public static int ReadInt(string message, int minValue = int.MinValue, int maxValue = int.MaxValue)
+        {
+            int result;
 
+            Console.Write(message);
+
+            while (int.TryParse(Console.ReadLine(), out result) == false || result < minValue || result >= maxValue)
+            {
+                Console.Error.WriteLine("Ошибка!. Попробуйте снова!");
+            }
+
+            return result;
+        }
     }
 
     static class Display
     {
-
+        public static void Print(string message, ConsoleColor consoleColor = ConsoleColor.White)
+        {
+            ConsoleColor defaultColor = Console.ForegroundColor;
+            Console.ForegroundColor = consoleColor;
+            Console.Write(message);
+            Console.ForegroundColor = defaultColor;
+        }
     }
 }
 
