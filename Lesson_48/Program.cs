45f2d1b65473f6c4f763d454b8d09590be6ad9a5
Maybeonce <a-u-rom@yandex.ru>
2023-11-16 12:02:18 +0300

    Аквариум

--- Lesson_48/Program.cs
+++ Lesson_48/Program.cs
@@ -115,6 +115,101 @@
             return _age;
         }
     }
+
+    #region UserUtils
+
+    static class Randomaizer
+    {
+        private static readonly Random s_random;
+
+        static Randomaizer()
+        {
+            s_random = new();
+        }
+
+        public static string GenerateRandomName(string[] names)
+        {
+            return names[s_random.Next(0, names.Length)];
+        }
+
+        public static int GenerateRandomNumber(int minValue, int maxValue)
+        {
+            return s_random.Next(minValue, maxValue);
+        }
+
+        public static List<T> Shuffle<T>(List<T> array)
+        {
+            int elementIndex;
+            T tempElement;
+
+            for (int i = 0; i < array.Count; i++)
+            {
+                elementIndex = GenerateRandomNumber(i, array.Count);
+
+                tempElement = array[i];
+                array[i] = array[elementIndex];
+                array[elementIndex] = tempElement;
+            }
+
+            return array;
+        }
+
+        public static bool IsСhanceNow(int chancePercent)
+        {
+            int minPercent = 0;
+            int maxPercent = 100;
+            int randomValue = GenerateRandomNumber(minPercent, maxPercent);
+
+            return randomValue < chancePercent;
+        }
+    }
+
+    static class Display
+    {
+        public static void Print(string message, ConsoleColor consoleColor = ConsoleColor.White)
+        {
+            ConsoleColor defaultColor = Console.ForegroundColor;
+            Console.ForegroundColor = consoleColor;
+            Console.Write(message);
+            Console.ForegroundColor = defaultColor;
+        }
+
+        public static void PrintLine(ConsoleColor color = ConsoleColor.White)
+        {
+            int symbolCount = Console.WindowWidth - 1;
+            Print($"{new string('-', symbolCount)}\n", color);
+        }
+    }
+
+    static class FishNamesDatabase
+    {
+        private static readonly string[] s_names;
+
+        static FishNamesDatabase()
+        {
+            s_names = new string[]
+            {
+                "Окунь",
+                "Лещ",
+                "Осётр",
+                "Форель",
+                "Щука",
+                "Сом",
+                "Анчоус",
+                "Сельдь",
+                "Лосось",
+                "Камбала",
+                "Карп",
+            };
+        }
+
+        public static string[] GetNames()
+        {
+            return s_names;
+        }
+    }
+
+    #endregion
 }
 
 //Аквариум
