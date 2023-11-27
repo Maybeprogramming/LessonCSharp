b387ae98c42f187292e63c9c8dcfeadbd92486d9
Maybeonce <a-u-rom@yandex.ru>
2023-11-27 14:05:08 +0300

    !

--- Lesson_51/Program.cs
+++ Lesson_51/Program.cs
@@ -1,7 +1,9 @@
-﻿using System.Text;
-
-namespace Lesson_51
+﻿namespace Lesson_51
 {
+    using static Display;
+    using static UserInput;
+    using System.Text;
+
     class Program
     {
         static void Main()
@@ -31,31 +33,31 @@ namespace Lesson_51
                 new Criminal("Казим Замалиев", false, 185, 60, "Татарин"),
                 new Criminal("Эльнар Атаев", false, 185, 60, "Узбек"),
                 new Criminal("Багымбай Бердиев", false, 160, 60, "Узбек"),
-                new Criminal("Заман Гаттаулин", false, 160, 60, "Турок"),
-                new Criminal("Ильдар Касимов", false, 160, 60, "Турок"),
+                new Criminal("Заман Гаттаулин", false, 160, 60, "Татарин"),
+                new Criminal("Ильдар Касимов", false, 160, 60, "Татарин"),
                 new Criminal("Армель Бюжо", false, 175, 60, "Француз"),
                 new Criminal("Гастон Дебюсси", false, 175, 60, "Француз"),
                 new Criminal("Микола Радчук", false, 175, 60, "Белорус"),
                 new Criminal("Тимур Савич", false, 170, 60, "Белорус"),
                 new Criminal("Анджей Любанский", false, 180, 60, "Поляк"),
                 new Criminal("Бартоломей Невядомский", false, 185, 60, "Поляк"),
-                new Criminal("Айан Галев", false, 175, 60, "Якут"),
-                new Criminal("Дохсун Жданов", false, 160, 60, "Якут")
+                new Criminal("Айан Галев", false, 175, 60, "Татарин"),
+                new Criminal("Дохсун Жданов", false, 160, 60, "Татарин")
             };
         }
 
-        public List<Criminal> TryGetCriminals(string[] nationaly, bool isImprisoned, int heigth, int weigth)
+        public List<Criminal> TryGetCriminals(int heigth, int weigth, string nationaly)
         {
             return new List<Criminal>(_criminals.Where(crimainal => crimainal.Height == heigth &&
                                                                     crimainal.Weight == weigth &&
-                                                                    (crimainal.Nationaly.Equals(nationaly[0]) || crimainal.Nationaly.Equals(nationaly[1])) &&
-                                                                    crimainal.IsImprisoned != isImprisoned));
+                                                                    crimainal.Nationaly.Equals(nationaly) &&
+                                                                    crimainal.IsImprisoned != true));
         }
 
         public string TryGetCriminalsInfo()
         {
             StringBuilder criminalsInfo = new StringBuilder();
-            List<Criminal> criminals = TryGetCriminals(new string[2] { "Русский", "Белорус" }, true, 170, 60);
+            List<Criminal> criminals = TryGetCriminals(170, 60, "Русский");
 
             foreach (var criminal in criminals)
             {
@@ -67,7 +69,17 @@ namespace Lesson_51
 
         public void Work()
         {
-            Console.WriteLine($"{TryGetCriminalsInfo()}");
+            Print($"{TryGetCriminalsInfo()}\n");
+        }
+
+        private void ShowAllCriminalsInfo()
+        {
+            Print($"Список всех преступников:\n");
+
+            for (int i = 0; i < _criminals.Count; i++)
+            {
+                Print($"{i + 1}. {_criminals[i].ShowInfo()}\n");
+            }
         }
     }
 
@@ -104,9 +116,74 @@ namespace Lesson_51
 
         public string ShowInfo()
         {
-            return $"{Name}. Рост [{Height}], Вес [{Weight}], Национальность: [{Nationaly}]. Заключение под стражей: [{IsImprisonedToString}].";
+            return $"{Name}. Рост [{Height}] см, Вес [{Weight}] кг, Национальность: [{Nationaly}]. Заключение под стражей: [{IsImprisonedToString}].";
         }
     }
+
+    #region UserUtils
+
+    static class Randomaizer
+    {
+        private static readonly Random s_random;
+
+        static Randomaizer()
+        {
+            s_random = new();
+        }
+
+        public static string GenerateRandomName(string[] names)
+        {
+            return names[s_random.Next(0, names.Length)];
+        }
+
+        public static int GenerateRandomNumber(int minValue, int maxValue)
+        {
+            return s_random.Next(minValue, maxValue);
+        }
+    }
+
+    static class UserInput
+    {
+        public static int ReadIntRange(string message, int minValue = int.MinValue, int maxValue = int.MaxValue)
+        {
+            int result;
+
+            Console.Write(message);
+
+            while (int.TryParse(Console.ReadLine(), out result) == false || result < minValue || result >= maxValue)
+            {
+                Console.Error.WriteLine("Ошибка!. Попробуйте снова!");
+            }
+
+            return result;
+        }
+
+        public static void WaitToPressKey(string message = "")
+        {
+            Print(message);
+            Print($"Для продолжения нажмите любую клавишу...\n");
+            Console.ReadKey();
+        }
+    }
+
+    static class Display
+    {
+        public static void Print(string message, ConsoleColor consoleColor = ConsoleColor.White)
+        {
+            ConsoleColor defaultColor = Console.ForegroundColor;
+            Console.ForegroundColor = consoleColor;
+            Console.Write(message);
+            Console.ForegroundColor = defaultColor;
+        }
+
+        public static void PrintLine(ConsoleColor color = ConsoleColor.White)
+        {
+            int symbolCount = Console.WindowWidth - 1;
+            Print($"{new string('-', symbolCount)}\n", color);
+        }
+    }
+
+    #endregion
 }
 
 //Поиск преступника
