1c152f2c7770aef382543d71d97be7b3cd6e1aa5
Maybeonce <a-u-rom@yandex.ru>
2023-11-28 10:44:20 +0300

    !

--- Lesson_52/Program.cs
+++ Lesson_52/Program.cs
@@ -2,27 +2,120 @@
 {
     using static Display;
     using static UserInput;
+    using static Randomaizer;
 
     class Program
     {
         static void Main()
         {
-            Console.Title = "Амнистия";
+            Console.Title = "Амнистия в стране Арстоцка";
+
+            int prisonersCount = 20;
+            Prison prison = new Prison(prisonersCount);
+            prison.Work();
+
+            WaitToPressKey("\nРабота программы завершена.\n");
         }
     }
 
     class Prison
     {
-        List<Prisoner> _prisoners;
+        private List<Prisoner> _prisoners;
 
-        public Prison()
+        public Prison(int prisonersCount)
         {
-            _prisoners = new List<Prisoner>()
+            _prisoners = FillPrisoners(prisonersCount);
+        }
+
+        public void Work()
+        {
+            const string HoldAnAmnestyCommand = "1";
+            const string ExitCommand = "2";
+
+            string crimeToAmnesty = "Антиправительственное";
+            string quastionText = $"\nВы хотите амнистировать преступников за {crimeToAmnesty}?\n";
+            string menu = $"{HoldAnAmnestyCommand} - Аминистировать\n" +
+                          $"{ExitCommand} - Не делать ничего и выйти из программы\n";
+            string requestMessage = "Введите номер команды: ";
+            string userInput;
+            bool isRun = true;
+
+            ShowPrisonersInfo("Список всех преступников:\n", _prisoners);
+
+            Print(quastionText, ConsoleColor.Yellow);
+            Print(menu);
+
+            while (isRun == true)
             {
-                new Prisoner("", ""),
-                new Prisoner("", ""),
-                new Prisoner("", ""),
+                userInput = ReadString(requestMessage);
+
+                switch (userInput)
+                {
+                    case HoldAnAmnestyCommand:
+                        ToHoldAnAmnesty(_prisoners);
+                        isRun = false;
+                        break;
+
+                    case ExitCommand:
+                        isRun = false;
+                        break;
+
+                    default:
+                        Print("Такой команды нет, попробуйте снова...\n", ConsoleColor.Red);
+                        break;
+                }
+            }
+        }
+
+        private void ToHoldAnAmnesty(List<Prisoner> prisoners)
+        {
+
+        }
+
+        private List<Prisoner> FillPrisoners(int prisonersCount)
+        {
+            List<Prisoner> prisoners = new List<Prisoner>();
+
+            string[] firstNames =
+            {
+                "Алексей", "Александр", "Вячеслав", "Всеволод", "Геннадий", "Григорий", "Дмитрий", "Даниил", "Демьян", "Михаил",
+                "Леонид", "Николай", "Валерий", "Сергей", "Иван", "Олег", "Владислав", "Игорь", "Юрий", "Павел", "Пётр", "Андрей"
             };
+
+            string[] lastNames =
+            {
+                "Алексеев", "Иванов", "Петров", "Павлов", "Жуков", "Михаленков", "Прудков", "Жабин", "Плотниченко", "Зайцев", "Сидоров",
+                "Володченко", "Сергеев", "Бубликов", "Пирожков", "Карченко", "Пухалёв", "Рожков", "Сабельников", "Пыжиков", "Стародубцев"
+            };
+
+            string[] crimesNames =
+            {
+                "Антиправительственное", "Коррупция", "Грабеж", "Хулиганство"
+            };
+
+            string fullName;
+            string crime;
+
+
+            for (int i = 0; i < prisonersCount; i++)
+            {
+                fullName = firstNames[GenerateRandomNumber(0, firstNames.Length)] + " " + lastNames[GenerateRandomNumber(0, lastNames.Length)];
+                crime = crimesNames[GenerateRandomNumber(0, crimesNames.Length)];
+
+                prisoners.Add(new Prisoner(fullName, crime));
+            }
+
+            return prisoners;
+        }
+
+        private void ShowPrisonersInfo(string message, List<Prisoner> prisoners)
+        {
+            Print(message, ConsoleColor.Green);
+
+            for (int i = 0; i < prisoners.Count; i++)
+            {
+                Print($"{i + 1}. {prisoners[i].ShowInfo()}\n");
+            }
         }
     }
 
@@ -41,11 +134,6 @@
         {
             return $"{Name}. Статья: {Crime}";
         }
-    }
-
-    enum Crimes
-    {
-
     }
 
     #region UserUtils
@@ -95,6 +183,21 @@
         {
             int symbolCount = Console.WindowWidth - 1;
             Print($"{new string('-', symbolCount)}\n", color);
+        }
+    }
+
+    static class Randomaizer
+    {
+        private static readonly Random s_random;
+
+        static Randomaizer()
+        {
+            s_random = new();
+        }
+
+        public static int GenerateRandomNumber(int minValue, int maxValue)
+        {
+            return s_random.Next(minValue, maxValue);
         }
     }
 
