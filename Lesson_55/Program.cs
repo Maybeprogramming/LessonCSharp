cc2f4e0429f18abfe9828b9d416df4e69b690b7b
Maybeonce <a-u-rom@yandex.ru>
2023-11-29 14:35:36 +0300

    Определение просрочки

--- Lesson_55/Program.cs
+++ Lesson_55/Program.cs
@@ -2,18 +2,27 @@
 {
     using static Display;
     using static UserInput;
+    using static Randomaizer;
 
     class Program
     {
         static void Main()
         {
             Console.Title = "Определение просрочки";
+
+            int productsCount = 15;
+            Stock stock = new Stock(productsCount);
+            stock.Work();
+
+            PrintLine();
+            Print($"Работа программы завершена!\n", ConsoleColor.Green);
         }
     }
 
     class Stock
     {
         List<Product> _products;
+        
 
         public Stock(int productsCount)
         {
@@ -22,22 +31,83 @@
 
         public void Work()
         {
+            int currentDate = 2023;
 
+            ShowProducts($"Список тушенки на складе:\n", _products);
+            PrintLine();
+
+            IdentifyExpiredProducts(_products, currentDate);
+            PrintLine();
+
+            WaitToPressKey("\n");
         }
 
-        private void ShowProducts()
+        private void IdentifyExpiredProducts(List<Product> products, int currentDate)
         {
+            List<Product> expiredProducts = new List<Product>();
 
+            expiredProducts = products.Where(product => GetExpiredDateProduct(product, currentDate) != null).ToList();
+
+            ShowProducts("Список просроченных банок тушенки:\n", expiredProducts);
+        }
+
+        private Product GetExpiredDateProduct(Product product, int currentDate)
+        {
+            int storagePeriod = currentDate - product.ProductionDate;
+            int remainingStorageTime = product.ExpirationDate - storagePeriod;
+
+            return product = remainingStorageTime < 0 ? product : null;
+        }
+
+        private void ShowProducts(string message, List<Product> products)
+        {
+            Print(message, ConsoleColor.Green);
+
+            for (int i = 0; i < products.Count; i++)
+            {
+                Print($"{i + 1}. {products[i]}\n");
+            }
         }
 
         private List<Product> FillProducts(int productsCount)
         {
-            return new List<Product>();
+            List<string> productsNames = new List<string>() 
+            { 
+                "Барс Экстра", "Главпродукт", "Батькин резерв", "Кронидов", "Гродфуд", "Село Зелёное", "Совок"
+            };
+
+            string name;
+            int productionDate;
+            int expirationDate;
+            int minProductionDate = 2000;
+            int maxProductionDate = 2023;
+            int minExpirationDate = 10;
+            int maxExpirationDate = 20;
+            List<Product> products = new List<Product>();
+
+            for (int i = 0; i < productsCount; i++)
+            {
+                name = productsNames[GenerateRandomNumber(0, productsNames.Count)];
+                productionDate = GenerateRandomNumber(minProductionDate, maxProductionDate + 1);
+                expirationDate = GenerateRandomNumber(minExpirationDate, maxExpirationDate + 1);
+                Product product = new Product(name, productionDate, expirationDate);
+
+                products.Add(product);
+            }
+
+            return products;
         }
     }
 
     class Product
     {
+        public Product(string name, int productionDate, int expirationDate)
+        {
+            Name = name;
+            ProductionDate = productionDate;
+            ExpirationDate = expirationDate;
+        }
+
         public string Name { get; }
         public int ProductionDate { get; }
         public int ExpirationDate { get; }
