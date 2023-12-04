50aa9520b9da8483a18cc906e349813c2832ebae
Maybeonce <a-u-rom@yandex.ru>
2023-12-04 15:06:30 +0300

    CarService

--- Lesson_50/Program.cs
+++ Lesson_50/Program.cs
@@ -3,14 +3,46 @@
     using static Randomaizer;
     using static Display;
     using static UserInput;
-    using System.Security.Cryptography;
-    using System.Threading.Tasks;
-    using System;
 
     class Program
     {
         static void Main()
         {
+            List<Detail> details = new List<Detail>()
+            {
+                new Engine(false),
+                new Transmission(false),
+                new Wheel(false),
+                new Glass(false),
+                new Muffler(false),
+                new Brake(false),
+                new Suspension(false),
+                new Generator(false),
+                new AirConditioner(false),
+                new Starter(false),
+                new TimingBelt(false),
+                new WaterPump(false),
+                new GasTank(false),
+                new SteeringWheel(false),
+                new SteeringRack(false),
+                new PowerSteering(false),
+                new Dashboard(false),
+                new Wiring(false),
+                new Battery(false),
+                new SparkPlug(false),
+                new FuelPump(false),
+                new OilFilter(false),
+                new Crankshaft(false),
+                new Catalyst(false)
+            };
+
+
+            for (int i = 0; i < details.Count; i++)
+            {
+                Print($"{i + 1}. {details[i].ShowInfo()}\n");
+            }
+
+            Console.ReadKey();
         }
     }
 
@@ -39,52 +71,392 @@
 
     }
 
-    //деталь
-    abstract class Detail
+    #region классы деталей
+
+    abstract class Detail : ICloneable
     {
-        public string Name { get; }
+        public Detail(bool isBroken)
+        {
+            IsBroken = isBroken;
+        }
+
+        public string Name { get => DetailsDictionary.TryGetName(GetType()); }
         public bool IsBroken { get; }
+        public virtual string IsBrokenToString { get => IsBroken == true ? "не исправен" : "исправен"; }
+
+        public abstract Detail Clone(bool isBroken);
+
+        public string ShowInfo()
+        {
+            return $"{Name}. Состояние: {IsBrokenToString}";
+        }
     }
 
     class Engine : Detail
     {
+        public Engine(bool isBroken) : base(isBroken)
+        {
+        }
 
+        public override Detail Clone(bool isBroken)
+        {
+            return new Engine(isBroken);
+        }
     }
 
     class Transmission : Detail
     {
+        public Transmission(bool isBroken) : base(isBroken)
+        {
+        }
 
+        public override Detail Clone(bool isBroken)
+        {
+            return new Transmission(isBroken);
+        }
     }
 
+    class Wheel : Detail
+    {
+        public Wheel(bool isBroken) : base(isBroken)
+        {
+        }
+
+        public override Detail Clone(bool isBroken)
+        {
+            return new Wheel(isBroken);
+        }
+    }
+
+    class Glass : Detail
+    {
+        public Glass(bool isBroken) : base(isBroken)
+        {
+        }
+
+        public override Detail Clone(bool isBroken)
+        {
+            return new Glass(isBroken);
+        }
+    }
+
+    class Muffler : Detail
+    {
+        public Muffler(bool isBroken) : base(isBroken)
+        {
+        }
+
+        public override Detail Clone(bool isBroken)
+        {
+            return new Muffler(isBroken);
+        }
+    }
+
+    class Brake : Detail
+    {
+        public Brake(bool isBroken) : base(isBroken)
+        {
+        }
+
+        public override Detail Clone(bool isBroken)
+        {
+            return new Brake(isBroken);
+        }
+    }
+
+    class Suspension : Detail
+    {
+        public Suspension(bool isBroken) : base(isBroken)
+        {
+        }
+
+        public override Detail Clone(bool isBroken)
+        {
+            return new Suspension(isBroken);
+        }
+    }
+
+    class Generator : Detail
+    {
+        public Generator(bool isBroken) : base(isBroken)
+        {
+        }
+
+        public override Detail Clone(bool isBroken)
+        {
+            return new Generator(isBroken);
+        }
+    }
+
+    class AirConditioner : Detail
+    {
+        public AirConditioner(bool isBroken) : base(isBroken)
+        {
+        }
+
+        public override Detail Clone(bool isBroken)
+        {
+            return new AirConditioner(isBroken);
+        }
+    }
+
+    class Starter : Detail
+    {
+        public Starter(bool isBroken) : base(isBroken)
+        {
+        }
+
+        public override Detail Clone(bool isBroken)
+        {
+            return new Starter(isBroken);
+        }
+    }
+
+    class TimingBelt : Detail
+    {
+        public TimingBelt(bool isBroken) : base(isBroken)
+        {
+        }
+
+        public override Detail Clone(bool isBroken)
+        {
+            return new TimingBelt(isBroken);
+        }
+    }
+
+    class WaterPump : Detail
+    {
+        public WaterPump(bool isBroken) : base(isBroken)
+        {
+        }
+
+        public override Detail Clone(bool isBroken)
+        {
+            return new WaterPump(isBroken);
+        }
+    }
+
+    class GasTank : Detail
+    {
+        public GasTank(bool isBroken) : base(isBroken)
+        {
+        }
+
+        public override Detail Clone(bool isBroken)
+        {
+            return new GasTank(isBroken);
+        }
+    }
+
+    class SteeringWheel : Detail
+    {
+        public SteeringWheel(bool isBroken) : base(isBroken)
+        {
+        }
+
+        public override Detail Clone(bool isBroken)
+        {
+            return new SteeringWheel(isBroken);
+        }
+    }
+
+    class SteeringRack : Detail
+    {
+        public SteeringRack(bool isBroken) : base(isBroken)
+        {
+        }
+
+        public override Detail Clone(bool isBroken)
+        {
+            return new SteeringRack(isBroken);
+        }
+    }
+
+    class PowerSteering : Detail
+    {
+        public PowerSteering(bool isBroken) : base(isBroken)
+        {
+        }
+
+        public override Detail Clone(bool isBroken)
+        {
+            return new PowerSteering(isBroken);
+        }
+    }
+
+    class Dashboard : Detail
+    {
+        public Dashboard(bool isBroken) : base(isBroken)
+        {
+        }
+
+        public override Detail Clone(bool isBroken)
+        {
+            return new Dashboard(isBroken);
+        }
+    }
+
+    class Wiring : Detail
+    {
+        public Wiring(bool isBroken) : base(isBroken)
+        {
+        }
+
+        public override Detail Clone(bool isBroken)
+        {
+            return new Wiring(isBroken);
+        }
+    }
+
+    class Battery : Detail
+    {
+        public Battery(bool isBroken) : base(isBroken)
+        {
+        }
+
+        public override Detail Clone(bool isBroken)
+        {
+            return new Battery(isBroken);
+        }
+    }
+
+    class SparkPlug : Detail
+    {
+        public SparkPlug(bool isBroken) : base(isBroken)
+        {
+        }
+
+        public override Detail Clone(bool isBroken)
+        {
+            return new SparkPlug(isBroken);
+        }
+    }
+
+    class FuelPump : Detail
+    {
+        public FuelPump(bool isBroken) : base(isBroken)
+        {
+        }
+
+        public override Detail Clone(bool isBroken)
+        {
+            return new FuelPump(isBroken);
+        }
+    }
+
+    class OilFilter : Detail
+    {
+        public OilFilter(bool isBroken) : base(isBroken)
+        {
+        }
+
+        public override Detail Clone(bool isBroken)
+        {
+            return new OilFilter(isBroken);
+        }
+    }
+
+    class Crankshaft : Detail
+    {
+        public Crankshaft(bool isBroken) : base(isBroken)
+        {
+        }
+
+        public override Detail Clone(bool isBroken)
+        {
+            return new Crankshaft(isBroken);
+        }
+    }
+
+    class Catalyst : Detail
+    {
+        public Catalyst(bool isBroken) : base(isBroken)
+        {
+        }
+
+        public override Detail Clone(bool isBroken)
+        {
+            return new Catalyst(isBroken);
+        }
+    }
+
+    #endregion
 
     #region Словарь деталей
 
-    class DetailsDictionary
+    static class DetailsDictionary
     {
-        // Колесо               Wheel
-        // Стекло               Glass
-        // Глушитель            Muffler
-        // Тормоз               Brake
-        // Подвеска             Suspension
-        // Трансмиссия          Transmission
-        // Двигатель            Engine
-        // Генератор            Generator
-        // Кондиционер          Air conditioner
-        // Стартер              Starter
-        // ГРМ                  timing belt
-        // Помпа                water pump
-        // Бензобак             Gas tank
-        // Руль                 Steering wheel
-        // Рулевая рейка        Steering rack
-        // Усилитель руля       Power steering
-        // Приборная панель     Dashboard
-        // Электропроводка      Wiring
-        // Аккумулятор          Battery
-        // Свеча зажигания      Spark plug
-        // Топливный насос      Fuel pump
-        // Масляный фильтр      Oil filter
-        // Коленвал             Crankshaft
-        // Катализатор          Catalyst
+        // Двигатель            Engine              +
+        // Трансмиссия          Transmission        +
+        // Колесо               Wheel               +
+        // Стекло               Glass               +
+        // Глушитель            Muffler             +
+        // Тормоз               Brake               +
+        // Подвеска             Suspension          +
+        // Генератор            Generator           +
+        // Кондиционер          AirConditioner      +
+        // Стартер              Starter             +
+        // ГРМ                  TimingBelt          +
+        // Водяная помпа        WaterPump           +
+        // Бензобак             GasTank             +
+        // Руль                 SteeringWheel       +
+        // Рулевая рейка        SteeringRack        +
+        // Усилитель руля       PowerSteering       +
+        // Приборная панель     Dashboard           +
+        // Электропроводка      Wiring              +
+        // Аккумулятор          Battery             +
+        // Свеча зажигания      SparkPlug           +
+        // Топливный насос      FuelPump            +
+        // Масляный фильтр      OilFilter           +
+        // Коленвал             Crankshaft          +
+        // Катализатор          Catalyst            +
+
+        private static Dictionary<Type, string> s_Details;
+
+        static DetailsDictionary()
+        {
+            s_Details = new Dictionary<Type, string>()
+            {
+                {typeof(Engine), "Двигатель"},
+                {typeof(Transmission), "Трансмиссия" },
+                {typeof(Wheel), "Колесо" },
+                {typeof(Glass), "Стекло" },
+                {typeof(Muffler), "Глушитель" },
+                {typeof(Brake), "Тормоз" },
+                {typeof(Suspension), "Подвеска" },
+                {typeof(Generator), "Генератор" },
+                {typeof(AirConditioner), "Кондиционер" },
+                {typeof(Starter), "Стартер" },
+                {typeof(TimingBelt), "ГРМ" },
+                {typeof(WaterPump), "Водяная помпа" },
+                {typeof(GasTank), "Бензобак" },
+                {typeof(SteeringWheel), "Руль" },
+                {typeof(SteeringRack), "Рулевая рейка" },
+                {typeof(PowerSteering), "Усилитель руля" },
+                {typeof(Dashboard), "Приборная панель" },
+                {typeof(Wiring), "Электропроводка" },
+                {typeof(Battery), "Аккумулятор" },
+                {typeof(SparkPlug), "Свеча зажигания" },
+                {typeof(FuelPump), "Топливный насос" },
+                {typeof(OilFilter), "Масляный фильтр" },
+                {typeof(Crankshaft), "Коленвал" },
+                {typeof(Catalyst), "Катализатор" },
+            };
+        }
+
+        public static string TryGetName(Type detail)
+        {
+
+            if (s_Details.TryGetValue(detail, out string name) == true)
+            {
+                return name;
+            }
+            else
+            {
+                return "Деталь";
+            }
+
+        }
     }
 
     #endregion
@@ -97,6 +469,11 @@
     interface IRepairProvider
     {
 
+    }
+
+    interface ICloneable
+    {
+        abstract Detail Clone(bool isBroken);
     }
 
     #region UserUtils
