99fe0b2fdbd9f2e4d57ca948ace0b1479e9cb87e
Maybeonce <a-u-rom@yandex.ru>
2024-01-24 14:46:55 +0300

    !

---
+++ Lesson_50_v2/Program.cs
@@ -0,0 +1,136 @@
+﻿using System.Diagnostics;
+using System.Reflection.Emit;
+using System;
+
+namespace Lesson_50_v2
+{
+    using static UserInput;
+    using static Randomaizer;
+    using static Display;
+
+    class Program
+    {
+        static void Main()
+        {
+        }
+    }
+
+    static class PartsDictionary
+    {
+        private static readonly List<string> s_PartsNames;
+
+        static PartsDictionary()
+        {
+            s_PartsNames = new List<string>()
+            {
+                "Двигатель",
+                "Трансмиссия",
+                "Колесо",
+                "Стекло",
+                "Глушитель",
+                "Тормоз",
+                "Подвеска",
+                "Генератор",
+                "Кондиционер",
+                "Стартер",
+                "ГРМ",
+                "Водяная помпа",
+                "Бензобак",
+                "Руль",
+                "Рулевая рейка",
+                "Усилитель руля",
+                "Приборная панель",
+                "Электропроводка",
+                "Аккумулятор",
+                "Свеча зажигания",
+                "Топливный насос",
+                "Масляный фильтр",
+                "Коленвал",
+                "Катализатор"
+            };
+        }       
+    }
+
+    static class Randomaizer
+    {
+        private static readonly Random s_random;
+
+        static Randomaizer()
+        {
+            s_random = new();
+        }
+
+        public static string GenerateRandomName(string[] names)
+        {
+            return names[s_random.Next(0, names.Length)];
+        }
+
+        public static int GenerateRandomNumber(int minValue, int maxValue)
+        {
+            return s_random.Next(minValue, maxValue);
+        }
+    }
+
+    static class UserInput
+    {
+        public static int ReadIntRange(string message, int minValue = int.MinValue, int maxValue = int.MaxValue)
+        {
+            int result;
+
+            Console.Write(message);
+
+            while (int.TryParse(Console.ReadLine(), out result) == false || result < minValue || result >= maxValue)
+            {
+                Console.Error.WriteLine("Ошибка!. Попробуйте снова!");
+            }
+
+            return result;
+        }
+
+        public static void WaitToPressKey(string message = "")
+        {
+            Print(message);
+            Print($"\nДля продолжения нажмите любую клавишу...");
+            Console.ReadKey();
+        }
+    }
+
+    static class Display
+    {
+        public static void Print(string message, ConsoleColor consoleColor = ConsoleColor.White)
+        {
+            ConsoleColor defaultColor = Console.ForegroundColor;
+            Console.ForegroundColor = consoleColor;
+            Console.Write(message);
+            Console.ForegroundColor = defaultColor;
+        }
+
+        public static void PrintLine(ConsoleColor color = ConsoleColor.White)
+        {
+            int symbolCount = Console.WindowWidth - 1;
+            Print($"{new string('-', symbolCount)}\n", color);
+        }
+    }
+}
+
+//Автосервис
+//У вас есть автосервис,
+//в который приезжают люди, чтобы починить свои автомобили.
+//У вашего автосервиса есть баланс денег и склад деталей.
+//Когда приезжает автомобиль,
+//у него сразу ясна его поломка,
+//и эта поломка отображается у вас в консоли вместе с ценой за починку
+//(цена за починку складывается из цены детали + цена за работу).
+//Поломка всегда чинится заменой детали,
+//но количество деталей ограничено тем,
+//что находится на вашем складе деталей.
+//Если у вас нет нужной детали на складе,
+//то вы можете отказать клиенту,
+//и в этом случае вам придется выплатить штраф.
+//Если вы замените не ту деталь,
+//то вам придется возместить ущерб клиенту.
+//За каждую удачную починку вы получаете выплату за ремонт,
+//которая указана в чек-листе починки.
+//Класс Деталь не может содержать значение “количество”.
+//Деталь всего одна, за количество отвечает тот, кто хранит детали.
+//При необходимости можно создать дополнительный класс для конкретной детали и работе с количеством.
